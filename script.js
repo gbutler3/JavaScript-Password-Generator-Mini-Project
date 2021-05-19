8037ac8a57cd9926fc5a0a7c86af093e7780ca58
grace butler <gmx.butler@gmail.com>
2021-05-18 23:13:19 -0500

    got the length prompt to work correctly

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); // looks for an id named generatebtn
+var capitals = "ABCDEFGHIJKLMNOP"
+var normal = "abcdefghijklmnop"
+var number ="0123456789"
+var specialSymbols = "!@#$%^&*()<>?*"
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); //this is a function being called - value expected to be returned 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,15 +15,12 @@ function writePassword() {
 }
 
 function generatePassword () {
-  var PWLength= prompt ("How many characters do you want to use (Must be between 8-128)");
-if (PWLength < 8){
-  alert ("Password must be at more than 8 characters");
-}
-else if (PWLength > 128){
-  alert ("Password must be less than 128 characters");
-}
-}
+  var ammount = prompt("How many characters do you want to use? (Must be between 8 and 128)");
+    if (ammount == null || ammount<8 || ammount>128 || isNaN(ammount)) {
+      alert("INVALID; Please enter a NUMBER between 8-128.");
+      return
+    }
+  }
 
-
-// Add event listener to generate button
+// Add event listener to generate button- listening for click and then it will rin "writepassword"
 generateBtn.addEventListener("click", writePassword);
