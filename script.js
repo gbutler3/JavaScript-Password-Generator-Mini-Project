2b77e920f19d82e73d035b2182d663db5468ecea
grace butler <gmx.butler@gmail.com>
2021-05-19 21:41:08 -0500

    in the process of setting conditions for the prompts. using console log to make sure the conditions are working, will probably take out the console.log's after everything is working?

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // looks for an id named generatebtn
 var capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Realized I had to put the split in there so that they wouldn't all be read together as one string
 var capitalsArray =capitals.split("");
 var normal = "abcdefghijklmnopqrstuvwxyz";
 var normalArray= normal.split("");
-var number ="0123456789";
+var number = "0123456789"
 var numberArray = number.split("");
 var specialSymbols = "!@#$%^&*()<>?+*";
 var specialSymbolsArray = specialSymbols.split("");
+var Inputs;//made this a variable because in the prompts I needed to define what the user input 
+var PWlength; //made this a variable because In the loop at the bottom I needed to define PWlength
+
 
 // Write password to the #password input
 function writePassword() {
@@ -19,8 +25,12 @@ function writePassword() {
 
 function generatePassword () {
   var PWlength = prompt("How many characters do you want to use? (Must be between 8 and 128)");
+  if (PWlength == null || PWlength<8 || PWlength>128 || isNaN(PWlength)) {
+    alert("INVALID; Please enter a NUMBER between 8-128.");
+    return}
+  //TO DO: take console.logs out after completed, using for visual of if code is working 
     console.log("password length " + PWlength)
-  var capitals = confirm("Would you like UPPERCASE letters in your password?");
+  var capitals = confirm("Would you like UPPERCASE letters in your password?"); 
     console.log(" capitals  " + capitals)
   var normal = confirm("Would you like LOWER CASE letters in your password?");
     console.log(" lower case  " + normal)
@@ -28,6 +38,38 @@ function generatePassword () {
     console.log(" numbers  " + number)
   var specialSymbols = confirm("Would you like SYMBOLS  in your password?");
     console.log("symbols " + specialSymbols)
+//this is if they user inputs cancel for all the options 
+ if(!capitals && !normal && !number && !specialSymbols) {
+   Inputs = alert ("No criteria selected, please start over")
+ } 
+//true CAP, LOWER, NUMBER, SYMBOL
+ else if(capitals && normal && number && specialSymbols){
+   Inputs= capitalsArray.concat(normalArray, numberArray, specialSymbolsArray);
+   console.log(Inputs);
+ }
+// true CAP, LOWER, NUMBER, 
+else if(capitals && normal && number){
+  Inputs= capitalsArray.concat(normalArray, numberArray);
+  console.log(Inputs);
 }
+// true LOWER, NUMBER, SYMBOL
+
+//true CAP, LOWER
+else if(capitals && normal){
+  Inputs= capitalsArray.concat(normalArray);
+  console.log(Inputs);
+}
+//true LOWER, NUMBER
+//true NUMBER, SYMBOL
+//true CAP
+else if(capitals){
+  Inputs= capitalsArray;
+  console.log(Inputs);
+}
+//true LOWER
+//true NUMBER
+//true SYMBOL
+}
+
 // Add event listener to generate button- listening for click and then it will rin "writepassword"
 generateBtn.addEventListener("click", writePassword);
